ca26b28dfd31d5253327df701346eca60e261f3c
Daniel-Damiba <danielddamiba@gmail.com>
2025-08-10 12:48:56 +0100

    rudementary version nearly finished

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: ddamiba <ddamiba@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 17:53:49 by ddamiba           #+#    #+#             */
-/*   Updated: 2025/08/09 13:49:18 by ddamiba          ###   ########.fr       */
+/*   Updated: 2025/08/10 12:45:41 by ddamiba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,15 @@ void free_arr(char **arr)
 
 int main(int argc, char **argv)
 {
-	if (argc < 3)
+	if (argc < 5)
 		return(perror("Insufficient args"), -1);
 	char *path = "/usr/bin/";
-    char **args1 = ft_split(argv[1], ' '); //{"ping", "-c", "5", "google.com", NULL};
-	char *cmd1 = ft_calloc(ft_strlen(path) + ft_strlen(args1[0]) + 1, sizeof(char));
-	ft_strlcpy(cmd1, path, ft_strlen(path) + ft_strlen(args1[0]) + 1);
-    ft_strlcat(cmd1, args1[0], ft_strlen(path) + ft_strlen(args1[0]) + 1);//"/usr/bin/ping";
+	char *temparg = ft_calloc(ft_strlen(argv[2]) + ft_strlen(argv[1]) + 2, sizeof(char));
+	ft_strlcpy(temparg, argv[2], ft_strlen(argv[2]) + ft_strlen(argv[1]) + 2);
+	ft_strlcat(temparg, " ", ft_strlen(argv[2]) + ft_strlen(argv[1]) + 2);
+	ft_strlcat(temparg, argv[1], ft_strlen(argv[2]) + ft_strlen(argv[1]) + 2);
+    char **args1 = ft_split(temparg, ' '); //{"ping", "-c", "5", "google.com", NULL};
+	char *cmd1 = ft_strjoin(path, args1[0]);
 	char *env_args1[] = {NULL};
 	int fd[2];
 	if (pipe(fd) == -1)
@@ -61,11 +63,12 @@ int main(int argc, char **argv)
 	}
 	free_arr(args1);
 	free(cmd1);
+	free(temparg);
 	
 	int pid2 = fork();
 	if (pid2 == -1)
 		return (perror("fork error\n"), 2);
-	char **args2 = ft_split(argv[2], ' ');
+	char **args2 = ft_split(argv[3], ' ');
     char *cmd2 = ft_calloc(ft_strlen(path) + ft_strlen(args2[0]) + 1, sizeof(char));
 	ft_strlcpy(cmd2, path, ft_strlen(path) + ft_strlen(args2[0]) + 1);
     ft_strlcat(cmd2, args2[0], ft_strlen(path) + ft_strlen(args2[0]) + 1);
