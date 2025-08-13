ab50a724501cc44b19220e46dea5496953c67dd3
Daniel-Damiba <danielddamiba@gmail.com>
2025-08-13 23:20:55 +0100

    a lot of stuff working, but main is far from finished

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: ddamiba <ddamiba@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 22:07:02 by ddamiba           #+#    #+#             */
-/*   Updated: 2025/08/13 00:51:35 by ddamiba          ###   ########.fr       */
+/*   Updated: 2025/08/13 23:10:29 by ddamiba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,11 +132,31 @@ void	cmd1(t_cmd *cmd_vars, int pipe[2], char *cmd_args, char **env)
 			return (clean_type_1(cmd_vars->file, *cmd_vars, pipe));
 		close(pipe[0]);
 		if (dup2(cmd_vars->fd, STDIN_FILENO) == -1)
-			return (perror("dup2 error"), cmd_clean(*cmd_vars), closed(pipe[1]), closed(cmd_vars->fd));
+			return (perror("child 1: dup2 error"), cmd_clean(*cmd_vars), closed(pipe[1]), closed(cmd_vars->fd));
 		close(cmd_vars->fd);
 		if (dup2(pipe[1], STDOUT_FILENO) == -1)
-			return (perror("dup2 error"), cmd_clean(*cmd_vars), closed(pipe[1]));
+			return (perror("child 1: dup2 error"), cmd_clean(*cmd_vars), closed(pipe[1]));
 		close(pipe[1]);
+		exec_cmd(*cmd_vars, env);
+	}
+}
+
+void	cmd_mid(t_cmd *cmd_vars, int pipes[2][2], char *cmd_args, char **env)
+{
+	cmd_vars->pid = fork();
+	if (cmd_vars->pid == -1)
+		return (ft_putstr_fd("fork error\n", 2));
+	if (cmd_vars->pid == 0)
+	{
+		if (!cmd_create(cmd_vars, cmd_args, env))
+			return (closefds(pipes[0]), closefds(pipes[1]), exit(127));
+		if (dup2(pipes[0][0], STDIN_FILENO) == -1)
+			return (perror("child mid: dup2 error"), cmd_clean(*cmd_vars), closed(pipes[0][0]), closed(cmd_vars->fd));
+		close(pipes[0][0]);
+		close(pipes[1][0]);
+		if (dup2(pipes[1][1], STDOUT_FILENO) == -1)
+			return (perror("child mid: dup2 error"), cmd_clean(*cmd_vars), closed(pipes[1][1]));
+		close(pipes[1][1]);
 		exec_cmd(*cmd_vars, env);
 	}
 }
@@ -153,12 +173,11 @@ void	cmd2(t_cmd *cmd_vars, int pipe[2], char *cmd_args, char **env)
 			return (perror(cmd_vars->file), closefds(pipe));
 		if (!cmd_create(cmd_vars, cmd_args, env))
 			return (close(cmd_vars->fd), closefds(pipe), exit(127));
-		close(pipe[1]);
 		if (dup2(pipe[0], STDIN_FILENO) == -1)
-			return (perror("dup2 error"), cmd_clean(*cmd_vars), closed(pipe[0]), closed(cmd_vars->fd));
+			return (perror("child 2: dup2 error"), cmd_clean(*cmd_vars), closed(pipe[0]), closed(cmd_vars->fd));
 		close(pipe[0]);
 		if (dup2(cmd_vars->fd, STDOUT_FILENO) == -1)
-			return (perror("dup2 error"), cmd_clean(*cmd_vars), closed(cmd_vars->fd));
+			return (perror("child 2: dup2 error"), cmd_clean(*cmd_vars), closed(cmd_vars->fd));
 		close(cmd_vars->fd);
 		exec_cmd(*cmd_vars, env);
 	}
@@ -166,31 +185,53 @@ void	cmd2(t_cmd *cmd_vars, int pipe[2], char *cmd_args, char **env)
 
 int	main(int argc, char **argv, char **env)
 {
-	t_cmd	cmd_vars[2];
-	int		fd[2];
+	t_cmd	cmd_vars[argc - 3];
+	int		fd[2][2];
+	int		i;
+	int		j;
 
 	if (argc < 5)
-		return (perror("Insufficient args"), -1);
-	if (pipe(fd) == -1)
+		return (ft_putstr_fd("Insufficient args\n", 2), -1);
+	if (pipe(fd[0]) == -1)
 		return (ft_putstr_fd("pipe error\n", 2), 1);
 	cmd_vars[0].file = argv[1];
-	cmd_vars[0].pid = -1;
-	cmd1(&cmd_vars[0], fd, argv[2], env);
+	cmd1(&cmd_vars[0], fd[0], argv[2], env);
 	if (cmd_vars[0].pid == 0)
 		exit(EXIT_FAILURE);
-	cmd_vars[1].file = argv[4];
-	cmd_vars[0].pid = -1;
-	cmd2(&cmd_vars[1], fd, argv[3], env);
-	if (cmd_vars[1].pid == 0)
+	if (cmd_vars[0].pid > 0)
+		close(fd[0][1]);
+	i = 3;
+	while(i < argc - 2)
+    {
+        // Create next pipe
+        if (pipe(fd[1]) == -1)
+            return (ft_putstr_fd("pipe error\n", 2), 1);
+
+        cmd_mid(&cmd_vars[i - 2], fd, argv[i], env);
+		if (cmd_vars[i - 2].pid == 0)
+			exit(EXIT_FAILURE);
+
+        // Close the previous pipe in the parent
+        close(fd[0][0]);
+    	close(fd[1][1]);
+
+        // Rotate: next pipe becomes previous
+        fd[0][0] = fd[1][0];
+        fd[0][1] = fd[1][1];
+		i++;
+    }
+	cmd_vars[i - 2].file = argv[argc - 1];
+	cmd2(&cmd_vars[i - 2], fd[0], argv[argc - 2], env);
+	if (cmd_vars[i - 2].pid == 0)
 		exit(EXIT_FAILURE);
-	closefds(fd);
-	waitpid(cmd_vars[1].pid, NULL, 0);
-	waitpid(cmd_vars[0].pid, NULL, 0);
-	if (cmd_vars[0].pid == 0)
-		ft_printf("Child 1 program done\n");
-	else if (cmd_vars[1].pid == 0)
-		ft_printf("Child 2 program done\n");
-	else
-		ft_printf("Main Program done\n");
+	if (cmd_vars[i - 2].pid > 0)
+		closefds(fd[0]);
+	j = i - 2;
+	while (j >= 0)
+	{
+		waitpid(cmd_vars[j].pid, NULL, 0);
+		j--;
+	}
+	ft_printf("Main Program done\n");
 	return (0);
 }
